614cf2dd895058421cc3453de838421532ff1fdd
Michael Lawrence Wyatt, II <mike@mikewyattdesign.com>
2013-08-11 18:25:46 -0500

    add main navigation partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,11 +3,11 @@ module ApplicationHelper
         @view_flow.set(:layout, output_buffer)
         self.output_buffer = render(:file => "layouts/#{layout}")
     end
-    def nav_link(link_text, link_path, force_active = false)
+    def nav_link(link_text, link_path, force_active = false, link_class ="")
         class_name = (force_active || current_page?(link_path)) ? 'active' : ''
         link_path << "?access_token=#{@access_token}" if @access_token
         content_tag(:li, class: class_name) do
-            link_to link_text, link_path
+            link_to link_text, link_path, class: link_class
         end
     end
 end
