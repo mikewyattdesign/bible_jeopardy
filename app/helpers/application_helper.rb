dfa384994e98da62171c4ed3233474d887aecf64
Michael Lawrence Wyatt, II <mike@mikewyattdesign.com>
2013-08-11 17:48:40 -0500

    add nav_link helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,13 @@
 module ApplicationHelper
     def parent_layout(layout)
-        @content_for_layout = self.output_buffer
+        @view_flow.set(:layout, output_buffer)
         self.output_buffer = render(:file => "layouts/#{layout}")
+    end
+    def nav_link(link_text, link_path, force_active = false)
+        class_name = (force_active || current_page?(link_path)) ? 'active' : ''
+        link_path << "?access_token=#{@access_token}" if @access_token
+        content_tag(:li, class: class_name) do
+            link_to link_text, link_path
+        end
     end
 end
