399af6bbe220e63a29b0534aaeda92a3eacdb2b9
Michael Wyatt <michaelwyatt@group360.com>
2014-05-17 15:36:35 -0500

    Remove invalid model specs and invalid factories

--- spec/factories/boards_categories.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :boards_category, :class => 'BoardsCategories' do
-    board nil
-    category nil
-    position 1
-  end
-end
--- spec/factories/boards_clues.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :boards_clue, :class => 'BoardsClues' do
-    board nil
-    clue nil
-    position 1
-  end
-end
