e504d759bcdf6b020ab203cbb37c42384f52a71e
Michael Wyatt <michaelwyatt@group360.com>
2014-05-18 09:20:06 -0500

    Activate Sauce if TRAVIS environment variable is set

--- spec/sauce_helper.rb
+++ spec/sauce_helper.rb
@@ -2,9 +2,21 @@
 # For options, check out http://saucelabs.com/docs/platforms
 require "sauce"
 require "sauce/capybara"
+require 'capybara/rails'
+require 'capybara/rspec'
+
+if ENV['TRAVIS']
+	Capybara.default_driver = :sauce 
+	Capybara.javascript_driver = :sauce
+else
+	Capybara.default_driver = :webkit
+	Capybara.javascript_driver = :selenium
+end
 
 # Setup configuration
 Sauce.config do |config|
+  config[:start_tunnel] = ENV["TRAVIS"]
+  config[:start_local_application] = ENV["TRAVIS"]
   config[:browsers] = [ 
     ["Windows 8", "Internet Explorer", "10"],             
     ["Windows 7", "Firefox", "20"],
