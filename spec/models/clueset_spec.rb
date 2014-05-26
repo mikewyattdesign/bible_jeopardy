1834078814643f4259f5798c210c1729dbc72b37
Michael Wyatt <michaelwyatt@group360.com>
2014-05-26 10:32:07 -0500

    Add clue set model test

--- spec/models/clueset_spec.rb
+++ spec/models/clueset_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Clueset do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it 'should respond to a board' do
+        expect(Clueset.new).to respond_to(:board)
+    end
+
+    it 'should respond to a clue' do
+        expect(Clueset.new).to respond_to(:clue)
+    end
 end
